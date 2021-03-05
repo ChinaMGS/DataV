d6380358ad71313110a88edb703cda0c1f560304
ma <mgs_zhongguo@163.com>
2021-03-05 18:07:38 +0800

    包

--- src/common/utils/pdfjs.js
+++
@@ -1,8 +0,0 @@
-// pdfjs 插件配置
-
-const version = '2.2.228'
-const pdfjsLib = require('../../../static/pdf/build/pdf.js')
-// pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${version}/pdf.worker.js`
-const CMAP_URL = `https://unpkg.com/pdfjs-dist@${version}/cmaps/`
-
-export { pdfjsLib, CMAP_URL }
